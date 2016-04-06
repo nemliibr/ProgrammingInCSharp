45ba29ac556938662a0ee1b49c151d7fd60ebfdd
nemliibr <nemliibr@gmail.com>
2016-04-06 23:08:25 +0200

    update

---
+++ ProgrammingInCSharp/Chapter1/Listing1_20WinForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Controls;
+using System.Windows.Forms.Integration;
+using System.Windows.Media;
+
+namespace ProgrammingInCSharp.Chapter1
+{
+    public partial class Listing1_20WinForm : Form
+    {
+        public Listing1_20WinForm()
+        {
+            InitializeComponent();
+        }
+
+        //Creates the WPF and show it on the WinForm on load event
+        private void Listing1_20WinFrom_Load(object sender, EventArgs e)
+        {
+            ElementHost host = new ElementHost();
+            host.Dock = DockStyle.Fill;
+
+            // Create the WPF UserControl.
+            Listing1_20WPF uc = new Listing1_20WPF();
+            host.Child = uc;
+            this.Controls.Add(host);
+        }
+    }
+}
